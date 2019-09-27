231841cf60541d9d8e9adca36abbca4a8b3558d0
Natalia Palacios <natalia.palacios-r@mail.escuelaing.edu.co>
2019-09-27 17:46:53 -0500

    Cliente listo

--- aws-lambda/src/main/java/edu/escuelaing/arep/MathServices.java
+++ aws-lambda/src/main/java/edu/escuelaing/arep/MathServices.java
@@ -1,25 +1,30 @@
 package edu.escuelaing.arep;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
 import static spark.Spark.*;
 
 import spark.Request;
 import spark.Response;
 
 public class MathServices {
-	
-	/**
-	 * 
-	 * @param args Command line arguments
-	 */
-	public static void main(String[] args) {        
-    	
-    	//setPort(4567);
+
+    /**
+     *
+     * @param args Command line arguments
+     */
+    public static void main(String[] args) {
+
+        //setPort(4567);
         port(getPort());
         get("/inputdata", (req, res) -> introducirDatos(req, res));
         get("/results", (req, res) -> operaciones(req, res));
     }
-	
-	private static String introducirDatos(Request req, Response res) {
+
+    private static String introducirDatos(Request req, Response res) {
         String pageContent
                 = "<!DOCTYPE html>"
                 + "<html>"
@@ -27,57 +32,61 @@ public class MathServices {
                 + "<h2>CUADRADO DE UN NUMERO</h2>"
                 + "<h3>Escriba el numero que desea elevar al cuadrado</h3>"
                 + "<form action=\"/results\">"
-                + "  Columna 1:<br>"
-                + "  <input type=\"text\" name=\"columna1\" value=\"\" required"
-                + "  <br><br>"
+                + "  Numero:<br>"
+                + "  <input type=\"text\" name=\"numero\" value=\"\" required>"
+                + "  </br>"
                 + "  <input type=\"submit\" value=\"Submit\">"
                 + "</form>"
                 + "</body>"
                 + "</html>";
-        
+
         return pageContent;
     }
-	
-	private static String operaciones(Request req, Response res) {
+
+    private static String operaciones(Request req, Response res) throws MalformedURLException, IOException {
         String pageContent;
-        String[] c1 = req.queryParams("columna1").split(" ");
-        double[] columna1 = leer(c1);
-        
-        URL pag = ("https://dnjydh351e.execute-api.us-east-1.amazonaws.com/Beta?value=" + columna1[0]);
+        pageContent
+                = "<!DOCTYPE html>"
+                + "<html>"
+                + "<body>"
+                + "<h2>El resultado es:</h2>"
+                + "<h4style=\"color:red;\">" + "CUADRADO: ";
         BufferedReader leeUrl = new BufferedReader(new InputStreamReader(System.in));
-		//System.out.println("Escriba la url");
-		//String urll = leeUrl.readLine();
-		//URL url = new URL(urll);
-		try (BufferedReader reader = new BufferedReader(new InputStreamReader(pag.openStream()))) {
-			String inputLine = null;
-			while ((inputLine = reader.readLine()) != null) {
-				p.println(inputLine);
-			}
-		} catch (IOException x) {
-			System.err.println(x);
-		}
-        
-        
-        
-        
-        
-        /*pageContent
-        = "<!DOCTYPE html>"
-        + "<html>"
-        + "<body>"
-        + "<h2>Los resultados son:</h2>"
-        + "<h3 style=\"color:red;\">Columna 1:</h3>"
-        + "<h4>"+"CUADRADO:"+"  "+String.format("%.2f", cuadrado)+"</h4>";
-        
-        return pageContent;*/
-		return null;
-	}
-	
-	
-	
-	
-    
-    public static Integer square(Integer i){
-        return i*i;
+        URL url = new URL("https://dnjydh351e.execute-api.us-east-1.amazonaws.com/Beta?value=" + req.queryParams("numero"));
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream()))) {
+            String inputLine = null;
+            while ((inputLine = reader.readLine()) != null) {
+                pageContent += inputLine;
+            }
+        } catch (IOException x) {
+            System.err.println(x);
+        }
+        pageContent += "</h4>";
+        return pageContent;
+    }
+
+    public static double[] leer(String leida[]) {
+        int size = leida.length;
+        double[] lista = new double[size];
+
+        for (int i = 0; i < size; i++) {
+            lista[i] = Double.parseDouble(leida[i]);
+        }
+
+        return lista;
+    }
+
+    /**
+     * This method reads the default port as specified by the PORT variable in
+     * the environment.
+     *
+     * Heroku provides the port automatically so you need this to run the
+     * project on Heroku.
+     */
+    static int getPort() {
+        if (System.getenv("PORT") != null) {
+            return Integer.parseInt(System.getenv("PORT"));
+        }
+        return 4567;
     }
 }
